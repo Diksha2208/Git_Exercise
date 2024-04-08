9ea7677c0851d0743add99c19e9a16056a87db5a
Diksha2208 <diksha2208@yahoo.com>
2024-04-08 16:29:25 -0700

    Feature to Sort Numbers

--- sortNumbers.js
+++ sortNumbers.js
@@ -3,6 +3,9 @@ function sortNumbers(array) {
     return array.sort();
 }
 
-console.log(sortNumbers([5, 2, 8, 9, 1])); // Outputs: [1, 2, 5, 8, 9]
+// input: [5, 2, 8, 9, 1]
+console.log(sortNumbers([5, 2, 8, 9, 1])); 
+
+// Outputs: [1, 2, 5, 8, 9]
 
 module.exports = sortNumbers;
