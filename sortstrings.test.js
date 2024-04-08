3e19a1b6be04d88e0847dd2ec1ad699c7ff5f2a3
Diksha2208 <diksha2208@yahoo.com>
2024-04-08 16:27:41 -0700

    Test for sort strings

--- sortstrings.test.js
+++ sortstrings.test.js
@@ -5,5 +5,9 @@ const sortStrings = require('./sortStrings.js');
 test('sorts an array of strings', () => {
     const input = ["apple", "banana", "mango", "cherry", "orange"];
     const output = ["apple", "banana", "cherry", "mango" , "orange"];
+    try {
     expect(sortStrings(input)).toEqual(output);
+    } catch (error) {
+        console.log(error);
+    }
 });
