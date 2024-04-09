3e3703b92735a6a54ff4d019d0ce0eb20f36c0fb
Diksha2208 <diksha2208@yahoo.com>
2024-04-08 20:51:00 -0700

    Test for Sort Numbers

--- sortNumbers.test.js
+++ sortNumbers.test.js
@@ -4,6 +4,8 @@ test('sortNumber should sort the array in ascending order', () => {
     const input = [5, 2, 8, 9, 1];
     const Output = [1, 2, 5, 8, 9];
 
+
+    // test sort number
 try{
     expect(sortNumber(input)).toEqual(Output);
 } catch (error) {
